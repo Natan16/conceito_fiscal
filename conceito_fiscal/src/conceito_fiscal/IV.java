425afbe1afd7e1ea50774a178ea7a2901341af83
Ftuyama <felipetuyama@gmail.com>
2015-10-25 17:18:00 -0300

    Adicionei Classes de Imposto e de P/S

--- conceito_fiscal/src/conceito_fiscal/IV.java
+++ conceito_fiscal/src/conceito_fiscal/IV.java
@@ -1,7 +1,17 @@
 package conceito_fiscal;
 
-public class IV {
+public class IV 
+{
 	private NF NF_;
+	private int quant_, desc_;
+	private AbstractPS PS_;
+	
+	public IV (NF nf, AbstractPS ps, int quant, int desc){
+		NF_ = nf;
+		PS_ = ps;
+		quant_ = quant;
+		desc_ = desc;
+	}
 
 	public NF getNF_() {
 		return NF_;
@@ -9,5 +19,17 @@ public class IV {
 
 	public void setNF_(NF nF_) {
 		NF_ = nF_;
+	}
+
+	public int getQuant_() {
+		return quant_;
+	}
+
+	public int getDesc_() {
+		return desc_;
+	}
+
+	public AbstractPS getPS_() {
+		return PS_;
 	}
 }
--- conceito_fiscal/src/conceito_fiscal/NF.java
+++ conceito_fiscal/src/conceito_fiscal/NF.java
@@ -1,18 +1,25 @@
 package conceito_fiscal;
 
+import imposto.CalcImposto;
 import java.util.ArrayList;
 
-public class NF {
+public class NF 
+{
 	private ArrayList<IV> IVs_;
+	private CalcImposto calcImposto_;
 	
 	public NF(){
 		IVs_ = new ArrayList<IV>();
+		calcImposto_ = new CalcImposto();
 	}
 	public void addIV(IV item){
 		IVs_.add(item);
 	}
 	public void removeIV(IV item){
 		IVs_.remove(item);
+	}
+	public int calculaImposto(){
+		return calcImposto_.tax(IVs_);
 	}
 }
 
