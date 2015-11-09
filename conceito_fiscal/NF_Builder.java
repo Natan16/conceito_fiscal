7c8d3ecde18e58acb06d0c0b4268dfb0f89617c3
joaovitormarques <joaovitormarques17@gmail.com>
2015-11-09 15:00:52 -0200

    Update NF_Builder.java

--- conceito_fiscal/NF_Builder.java
+++ conceito_fiscal/NF_Builder.java
@@ -4,14 +4,14 @@ import banco_dados.BDPS_Facade;
 
 public class NF_Builder 
 {
-	// Builder b�sico de NF
+	// Builder basico de NF
 	public static NF constructNF(){
 		NF nf_ = new NF();
 		PS_Abstract ps_ = BDPS_Facade.createNewPS("produto","A", null);
 		nf_.addNewIV(ps_, 1, 10);
 		return nf_;
 	}
-	// Builder b�sico de NF_Final
+	// Builder basico de NF_Final
 	public static NF_Final constructNF_Final(NF nf, int id){
 		NF_Final nf_ = new NF_Final(nf, id);
 		return nf_;
